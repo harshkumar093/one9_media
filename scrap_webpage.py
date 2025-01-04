741964d1ee8250d7f5266e3c8b8e871faee1ae31
HARSH KUMAR <55792055+harshkumar093@users.noreply.github.com>
2025-01-04 11:59:54 +0530

    Scrap webpage

--- scrap_webpage.py
+++ scrap_webpage.py
@@ -4,9 +4,9 @@ from bs4 import BeautifulSoup
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
-urls=[]
 
 def scrap_webpage(url, class_name, skip, limit):
+    urls=[]
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, "html.parser")
